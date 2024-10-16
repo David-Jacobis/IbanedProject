4d46a30ca329015f0c70e5b1cd3153f689233a4a
David-Jacobis <david.jacobis@outlook.com>
2024-10-15 21:47:47 -0300

    Ajustes e adicionando vídeo responsivo em tela.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,114 @@
 import "./Home.css";
 import React from "react";
-
+import VideoSemear from "../../assets/images/Biblia.MP4";
 import imagemTeste from "../../assets/images/Intercecao.jpg";
+
 import { motion } from "framer-motion";
-import { Divider, Flex, Card, Col, Row, Button } from "antd";
+import { useState, useRef } from "react";
+import { Flex, Card, Col, Row, Button } from "antd";
+import { faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import Button from "../button/Button";
-// import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-// import YouTubeIcon from "@mui/icons-material/YouTube";
-// import Video from "../../assets/images/Biblia.MP4";
 
-// import { Row, Col } from "antd";
+const Home = () => {
+  const [isMuted, setIsMuted] = useState(true); // Estado para controlar o som
+  const videoRef = useRef(null); // Referência para o vídeo
 
-function Home() {
+  // Função para alternar o som
+  const toggleMute = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted;
+      setIsMuted(!isMuted);
+    }
+  };
+
   return (
     <React.Fragment>
       <div className="content">
-        <Divider />
+        <div
+          className="VideoCard-Wrapper"
+          style={{ position: "relative", width: "100%", height: "70vh" }}
+        >
+          <video
+            ref={videoRef}
+            autoPlay
+            loop
+            muted={isMuted}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover", // Coloca o vídeo no fundo
+            }}
+          >
+            <source src={VideoSemear} type="video/mp4" />
+          </video>
+
+          <button
+            onClick={toggleMute}
+            style={{
+              position: "absolute",
+              top: "20px",
+              right: "20px",
+              background: "transparent",
+              border: "none",
+              color: "#fff",
+              fontSize: "24px",
+              cursor: "pointer",
+              zIndex: 2, // Coloca o botão acima do vídeo
+            }}
+          >
+            <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
+          </button>
+          <Flex
+            className="Flex-Text-Wrap MobileCenter"
+            vertical
+            justify="flex-start"
+            align="flex-start"
+            style={{
+              zIndex: 1,
+              position: "relative",
+              height: "100%",
+              padding: "24px 44px",
+            }}
+            gap="middle"
+          >
+            <h3>BEM-VINDO À NOSSA IGREJA</h3>
+            <h1>2024 É O ANO DE SEMEAR</h1>
+            <Flex
+              vertical
+              justify="center"
+              align="flex-start"
+              className="Flex-Text-Content"
+            >
+              <p>
+                Porque o que semeia para a sua própria carne da carne colherá
+                corrupção mas o que semeia para o Espírito do Espírito colherá
+                vida eterna.E não nos cansemos de fazer o bem, porque a seu
+                tempo ceifaremos, se
+              </p>
+              <p>
+                não desfalecermos.Por isso, enquanto tivermos oportunidade,
+                façamos o bem a todos, mas principalmente aos da família da fé.
+              </p>
+              <p>Gálatas 6:8-10</p>
+            </Flex>
+            <motion.a
+              className="Motionbutton"
+              whileHover={{ scale: 1.1 }}
+              href="https://wa.me/553125264248"
+              target="_blank" // Abre o link em uma nova aba
+              rel="noopener noreferrer"
+            >
+              <span className="ButtonContent">
+                <FontAwesomeIcon icon={faWhatsapp} />
+                MANDE SUA MENSAGEM
+              </span>
+            </motion.a>
+          </Flex>
+        </div>
         <div className="Group2">
           <h4>QUEM SOMOS?</h4>
           <h1>
@@ -91,10 +182,14 @@ function Home() {
             <h1>
               <strong>AMOR E COMPAIXÃO</strong>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo accusamus sunt inventore sapiente quia dicta.
-                Consequuntur mollitia quaerat cupiditate, perspiciatis qui,
-                omnis et hic voluptas obcaecati minima expedita iste maxime?
+                O amor, na Bíblia, é a base dos mandamentos, representado pelo
+                amor a Deus e ao próximo (Mateus 22:37-39). Ele é paciente,
+                bondoso e sacrificial (1 Coríntios 13:4-7), exemplificado pelo
+                sacrifício de Jesus por amor à humanidade (João 3:16). A
+                compaixão é a sensibilidade às necessidades dos outros, levando
+                à ação, como demonstrado por Jesus ao curar e ajudar aqueles que
+                sofriam (Mateus 9:36). Ambos são reflexos do caráter de Deus e
+                devem ser praticados pelos cristãos (Colossenses 3:12).
               </p>
             </h1>
           </Flex>
@@ -108,7 +203,7 @@ function Home() {
                   className="ImgCardCouple"
                   alt="example"
                   src={imagemTeste}
-                  style={{ objectFit: "cover"}}
+                  style={{ objectFit: "cover" }}
                 />
               </motion.div>
             </Col>
@@ -122,7 +217,7 @@ function Home() {
                   className="ImgCardGroup"
                   alt="example"
                   src={imagemTeste}
-                  style={{objectFit: "cover" }}
+                  style={{ objectFit: "cover" }}
                 />
               </motion.div>
             </Col>
@@ -141,142 +236,8 @@ function Home() {
             </Col>
           </Row>
         </div>
-        {/* <div className="box1">
-                <div className="text-div">
-                    <h1>Semear</h1>
-                    <h3>O ano de 2024 é o ano de Semear</h3>
-                    <p>
-                    ⁸ Quem semeia para a sua carne, da carne colherá destruição; mas quem semeia para o Espírito, do Espírito colherá a vida eterna.
-                    ⁹ E não nos cansemos de fazer o bem, pois no tempo próprio colheremos, se não desanimarmos.
-                    ¹⁰ Portanto, enquanto temos oportunidade, façamos o bem a todos, especialmente aos da família da fé.
-                    <br/>
-                    Gálatas 6:8-10
-                    </p>
-                    <p>Venha conosco neste ano e participe dos nossos cultos!</p>
-                </div>
-                <div className="button1">
-                    <Button 
-                    icon={<WhatsAppIcon fontSize="large" />}
-                    button="Mande sua Mensagem" 
-                    link="https://wa.me/553125264248?text=Vim%20atrav%C3%A9s%20do%20Site%20e%20gostaria%20de%20saber%20mais%20sobre%20o%20ano%20Semear."/>
-                </div>
-            </div>
-        <Row justify="center">
-          <Col sm={12} md={12} lg={24}>
-            <div className="video-container">
-              <video controls autoPlay loop>
-                <source src="movie.ogg" type="video/ogg" />
-                <source src={Video} type="video/mp4" />
-              </video>
-              <div className="caption">
-                <h1>Semear</h1>
-              </div>
-              <div className="button1">
-                <Button
-                  icon={<WhatsAppIcon fontSize="large" />}
-                  button="Mande sua Mensagem"
-                  link="https://wa.me/553125264248?text=Vim%20atrav%C3%A9s%20do%20Site%20e%20gostaria%20de%20saber%20mais%20sobre%20o%20ano%20Semear."
-                />
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Row justify="center">
-          <Col sm={6} md={6} lg={6}>
-            <div className="box2">
-              <div className="text-div">
-                <h1>Veja nossa Programação</h1>
-                <p>
-                  Abraçe a oportunidade de estar em comunhão com a igreja de
-                  Cristo.
-                </p>
-                <p>
-                  Confira nossos cronogramas e realize seu planejamento para
-                  poder participar.
-                </p>
-                <span>
-                  <strong>Segunda | 19h</strong>
-                </span>
-                <p>
-                  Acontece aqui em nosso prédio o programa Valorizando Vidas,
-                  <br /> voltado para quem precisa de ajuda em relação a vicíos
-                  e compulsões.
-                </p>
-                <span>
-                  <strong>Quarta | 20h</strong>
-                </span>
-                <p>
-                  Acontece o culto 30 semanas, venha participar e seja
-                  surpreendido pelo Espírito Santo.
-                </p>
-                <span>
-                  <strong>Domingo | 10:30h & 18h</strong>
-                </span>
-                <p>Acontece aqui em nosso prédio o culto de Domingo</p>
-                <div className="spanlink">
-                  <span>
-                    <strong>
-                      Assista a transmissão dos cultos de domingo no Youtube
-                    </strong>
-                  </span>
-                  <div className="button1">
-                    <Button
-                      icon={<YouTubeIcon fontSize="large" />}
-                      button="Assista Aqui"
-                      link="https://www.youtube.com/@IBANED"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-
-        <div className="box3">
-          <div className="text-div">
-            <h1>Intercessão</h1>
-            <h3>Participe do nosso ministério de Intercessão</h3>
-            <p>
-              “Todavia atende à oração do teu servo, e à sua súplica, ó Senhor
-              meu Deus, para ouvires o clamor e a oração que o teu servo faz
-              perante ti hoje; […] e ouve a súplica do teu servo, e do teu povo
-              Israel, quando orarem voltados para este lugar; sim, ouve tu no
-              lugar da tua habitação, nos céus; ouve, e perdoa.”
-              <br />1 Reis 8:28-30
-            </p>
-          </div>
-        </div>
-
-        <div className="box4">
-          <div className="text-div">
-            <h1>Avalanche</h1>
-            <h3>Participe do nosso ministério de Lutas</h3>
-            <p>
-              “Assim, quer vocês comam, quer bebam, quer façam qualquer outra
-              coisa, façam tudo para a glória de Deus.”
-              <br />
-              1Coríntios 10:31
-            </p>
-          </div>
-          <div className="button1">
-            <Button
-              button="Cadastre-se"
-              link="https://docs.google.com/spreadsheets/d/1GE1MpSpV3lC-ZLG8JDLJmZ_JqIXWAIQ5B23afzMT84g/edit#gid=0"
-            />
-          </div>
-        </div>
-
-        <div className="box5">
-          <div className="text-div">
-            <h1>Conheça nossos Ministérios</h1>
-          </div>
-          <div className="button1">
-            <Button button="Conheça Aqui" link="/Ministerios" />
-          </div>
-        </div> */}
       </div>
     </React.Fragment>
   );
-}
-
+};
 export default Home;
