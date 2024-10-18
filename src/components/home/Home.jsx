5d37886f4d7f4c20d50424550dd295a4b397d44b
David-Jacobis <david.jacobis@outlook.com>
2024-10-17 21:28:21 -0300

    fix: Ajustes e adição de novos conteúdos ao site.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,21 @@
 import "./Home.css";
 import React from "react";
 import VideoSemear from "../../assets/images/Biblia.MP4";
-import imagemTeste from "../../assets/images/Intercecao.jpg";
 
 import { motion } from "framer-motion";
 import { useState, useRef } from "react";
-import { Flex, Card, Col, Row, Button } from "antd";
-import { faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { Flex, Card, Col, Row } from "antd";
+import {
+  faVolumeMute,
+  faVolumeUp,
+  faCross,
+  faPeopleRoof,
+  faChurch,
+  faTree,
+  faEarthAmericas,
+  faQuoteLeft,
+  faQuoteRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -114,127 +123,155 @@ const Home = () => {
           <h1>
             <strong>DESCUBRA QUEM SOMOS</strong>
           </h1>
-          <Row gutter={[16, 16]} justify="center">
+          <Row gutter={[32, 48]} justify="center">
             <Col xs={24} sm={12} md={8}>
-              <Card className="CardGroup2">
+              <Card
+                className="CardGroup2"
+                style={{ backgroundColor: "#7ee787" }}
+              >
                 <div className="CardContent">
                   <FontAwesomeIcon
-                    icon={faWhatsapp}
+                    icon={faCross}
                     size="2x"
                     className="CardIcon"
+                    style={{ color: "#2ea043" }}
                   />
-                  <h2>
-                    <strong>SOBRE NÓS</strong>
-                  </h2>
-                  <p>
-                    Raízes e Tradições Explore a trajetória e os marcos
-                    importantes que moldaram nossa igreja ao longo dos anos.
-                  </p>
+                  <h2>EXISTIMOS PARA A GLÓRIA DE</h2>
+                  <h1>
+                    <strong>DEUS</strong>
+                  </h1>
                 </div>
               </Card>
-              <div className="CardGroup2Footer"></div>
+              <div
+                className="CardGroup2Footer"
+                style={{ backgroundColor: "#3fb950" }}
+              ></div>
+            </Col>
+
+            <Col xs={24} sm={12} md={8}>
+              <Card
+                className="CardGroup2"
+                style={{ backgroundColor: "#ffa198" }}
+              >
+                <div className="CardContent">
+                  <FontAwesomeIcon
+                    icon={faPeopleRoof}
+                    size="2x"
+                    className="CardIcon"
+                    style={{ color: "#da3633" }}
+                  />
+                  <h2>E POR ISSO FAZEMOS DISCÍPULOS DE</h2>
+                  <h1>
+                    <strong>JESUS CRISTO</strong>
+                  </h1>
+                </div>
+              </Card>
+              <div
+                className="CardGroup2Footer"
+                style={{ backgroundColor: "#f85149" }}
+              ></div>
             </Col>
 
             <Col xs={24} sm={12} md={8}>
               <Card className="CardGroup2">
                 <div className="CardContent">
                   <FontAwesomeIcon
-                    icon={faWhatsapp}
+                    icon={faChurch}
                     size="2x"
                     className="CardIcon"
                   />
-                  <h2>
-                    <strong>FAÇA PARTE</strong>
-                  </h2>
-                  <p>
-                    Participe e Cresça Conosco. Conheça as oportunidades de
-                    envolvimento e descubra como você pode se integrar à nossa
-                    família espiritual.
-                  </p>
+                  <h2>INCLUINDO OS NA FAMÍLIA DA</h2>
+                  <h1>
+                    <strong>FÉ</strong>
+                  </h1>
                 </div>
               </Card>
               <div className="CardGroup2Footer"></div>
             </Col>
-
             <Col xs={24} sm={12} md={8}>
-              <Card className="CardGroup2">
+              <Card
+                className="CardGroup2"
+                style={{ backgroundColor: "#b9efff" }}
+              >
                 <div className="CardContent">
                   <FontAwesomeIcon
-                    icon={faWhatsapp}
+                    icon={faTree}
                     size="2x"
                     className="CardIcon"
+                    style={{ color: "#2cc8f7" }}
                   />
-                  <h2>
-                    <strong>NOSSA MISSÃO</strong>
-                  </h2>
-                  <p>
-                    Unidos na Fé e no Amor. Descubra os objetivos e valores que
-                    guiam nossa comunidade e como buscamos fazer a diferença no
-                    mundo.
-                  </p>
+                  <h2>DESENVOLVENDO SEUS DONS E</h2>
+                  <h1>
+                    <strong>MINISTÉRIOS</strong>
+                  </h1>
                 </div>
               </Card>
-              <div className="CardGroup2Footer"></div>
+              <div
+                className="CardGroup2Footer"
+                style={{ backgroundColor: "#2cc8f7" }}
+              ></div>
+            </Col>
+            <Col xs={24} sm={12} md={8}>
+              <Card
+                className="CardGroup2"
+                style={{ backgroundColor: "#DDD6FE" }}
+              >
+                <div className="CardContent">
+                  <FontAwesomeIcon
+                    icon={faEarthAmericas}
+                    size="2x"
+                    className="CardIcon"
+                    style={{ color: "#A78BFA" }}
+                  />
+                  <h2>PARA A GLÓRIA GLOBAL DE</h2>
+                  <h1>
+                    <strong>DEUS</strong>
+                  </h1>
+                </div>
+              </Card>
+              <div
+                className="CardGroup2Footer"
+                style={{ backgroundColor: "#A78BFA" }}
+              ></div>
             </Col>
           </Row>
-          <Flex vertical>
-            <h2>Sub-headline</h2>
+        </div>
+        <div className="StyledFlex">
+          <div className="StyledFlex-Content">
             <h1>
-              <strong>AMOR E COMPAIXÃO</strong>
-              <p>
-                O amor, na Bíblia, é a base dos mandamentos, representado pelo
-                amor a Deus e ao próximo (Mateus 22:37-39). Ele é paciente,
-                bondoso e sacrificial (1 Coríntios 13:4-7), exemplificado pelo
-                sacrifício de Jesus por amor à humanidade (João 3:16). A
-                compaixão é a sensibilidade às necessidades dos outros, levando
-                à ação, como demonstrado por Jesus ao curar e ajudar aqueles que
-                sofriam (Mateus 9:36). Ambos são reflexos do caráter de Deus e
-                devem ser praticados pelos cristãos (Colossenses 3:12).
-              </p>
+              QUEREMOS <br />
+              SERVIR O MUNDO
+              <br /> AO NOSSO REDOR
             </h1>
-          </Flex>
-          <Row gutter={[16, 16]} justify="center">
-            <Col xs={24} sm={12} md={8}>
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                transition={{ duration: 0.3 }}
-              >
-                <img
-                  className="ImgCardCouple"
-                  alt="example"
-                  src={imagemTeste}
-                  style={{ objectFit: "cover" }}
-                />
-              </motion.div>
-            </Col>
-            <Col xs={24} sm={12} md={8}>
-              <Button>SAIBA MAIS</Button>
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                transition={{ duration: 0.3 }}
-              >
-                <img
-                  className="ImgCardGroup"
-                  alt="example"
-                  src={imagemTeste}
-                  style={{ objectFit: "cover" }}
-                />
-              </motion.div>
-            </Col>
-            <Col xs={24} sm={12} md={8}>
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                transition={{ duration: 0.3 }}
-              >
-                <img
-                  className="ImgCardCouple"
-                  alt="example"
-                  src={imagemTeste}
-                  style={{ objectFit: "cover" }}
-                />
-              </motion.div>
-            </Col>
-          </Row>
+            <FontAwesomeIcon
+              icon={faQuoteLeft}
+              size="3x"
+              className="CardIcon"
+              style={{ color: "#A78BFA", marginTop: "20px", alignSelf: 'flex-start' }} // Estilizando as aspas
+            />
+            <p>
+              Queremos servir o mundo ao nosso redor, pois fomos chamados a ser
+              luz e sal na Terra (Mateus 5:13-14). A fé cristã nos ensina que o
+              amor ao próximo é o maior mandamento depois de amar a Deus (Mateus
+              22:37-39). Servir é um ato de obediência e gratidão, pois “o Filho
+              do Homem não veio para ser servido, mas para servir” (Marcos
+              10:45). Ao cuidar dos necessitados, estamos servindo ao próprio
+              Cristo, como Ele disse: "Tudo o que fizerem a um destes meus
+              pequeninos irmãos, a mim o fizeram" (Mateus 25:40). Nosso chamado
+              é impactar vidas com gestos de compaixão, justiça e bondade. Assim
+              como Jesus lavou os pés de seus discípulos (João 13:14-15),
+              devemos seguir Seu exemplo de humildade e serviço. Em cada ato de
+              amor, refletimos o coração de Deus, levando esperança e
+              transformação ao mundo. Que possamos viver como instrumentos da
+              Sua paz e justiça, sendo mãos e pés de Cristo em nosso cotidiano.
+            </p>
+            <FontAwesomeIcon
+              icon={faQuoteRight}
+              size="3x"
+              className="CardIcon2"
+              style={{ color: "#A78BFA", marginTop: "20px", alignSelf: 'flex-end' }}
+            />
+          </div>
         </div>
       </div>
     </React.Fragment>
