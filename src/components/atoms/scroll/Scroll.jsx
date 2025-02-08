9bfe2c3d232db2135724250e401093b96f3ca573
David-Jacobis <david.jacobis@outlook.com>
2025-02-08 11:37:29 -0300

    feat: organização de componentes.

---
+++ src/components/atoms/scroll/Scroll.jsx
@@ -0,0 +1,29 @@
+import { motion, useSpring, useScroll } from "motion/react"
+
+export default function ScrollLinked() {
+    const { scrollYProgress } = useScroll()
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    })
+
+    return (
+        <>
+            <motion.div
+                id="scroll-indicator"
+                style={{
+                    scaleX,
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    height: 10,
+                    originX: 0,
+                    backgroundColor: "#ff0088",
+                    zIndex: 50
+                }}
+            />
+        </>
+    )
+}
