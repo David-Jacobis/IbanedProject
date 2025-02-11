1a2b772f51330ed6fcd4c3597069c0ffbb880392
David-Jacobis <david.jacobis@outlook.com>
2025-02-11 18:33:07 -0300

    feat: Adição de novas partes em programação

--- src/components/programacao/Programacao.jsx
+++ src/components/programacao/Programacao.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Programacao.css";
 import ScrollLinked from "../atoms/scroll/Scroll";
-import Imagem from "../../assets/images/Avalanche.jpg";
+import Imagem from "../../assets/images/2025  ANO DE fazer.png";
+import Semanas from "../../assets/images/TE AGRADEÇO - KLEBER LUCAS.png"
+import Oracao from "../../assets/images/oração.png"
+import Sow from "../../assets/images/SowTeste.png"
 
 import { Flex } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock, faLocation, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Programacao = () => {
   return (
@@ -21,28 +24,28 @@ const Programacao = () => {
           {/* Texto */}
           <div className="card-text">
             <h3><strong>CULTO 30 SEMANAS</strong></h3>
-            <h5> <FontAwesomeIcon icon={faClock} /> QUARTAS-FEIRA 20HRS</h5>
+            <h5> <FontAwesomeIcon icon={faClock} /> TODA QUARTA-FEIRA 20HRS</h5>
             <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
             <h5>Palmeiras, Belo Horizonte</h5>
           </div>
 
           {/* Imagem */}
           <div className="card-image">
-            <img src={Imagem} alt="Imagem do Card" />
+            <img src={Semanas} alt="Imagem do Card" />
           </div>
         </div>
         <div className="card">
           {/* Texto */}
           <div className="card-text">
             <h3><strong>CULTO DE ORAÇÃO</strong></h3>
-            <h5> <FontAwesomeIcon icon={faClock} /> QUINTAS-FEIRA 20HRS</h5>
+            <h5> <FontAwesomeIcon icon={faClock} /> TODA QUINTA-FEIRA 20HRS</h5>
             <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
             <h5>Palmeiras, Belo Horizonte</h5>
           </div>
 
           {/* Imagem */}
           <div className="card-image">
-            <img src={Imagem} alt="Imagem do Card" />
+            <img src={Oracao} alt="Imagem do Card" />
           </div>
         </div>
 
@@ -50,14 +53,14 @@ const Programacao = () => {
           {/* Texto */}
           <div className="card-text">
             <h3><strong>CULTO SOW</strong></h3>
-            <h5> <FontAwesomeIcon icon={faClock} /> SÁBADOS 19:30HRS</h5>
+            <h5> <FontAwesomeIcon icon={faClock} /> TODO SÁBADO 19:30HRS</h5>
             <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
             <h5>Palmeiras, Belo Horizonte</h5>
           </div>
 
           {/* Imagem */}
           <div className="card-image">
-            <img src={Imagem} alt="Imagem do Card" />
+            <img src={Sow} alt="Imagem do Card" />
           </div>
         </div>
 
@@ -65,7 +68,7 @@ const Programacao = () => {
           {/* Texto */}
           <div className="card-text">
             <h3><strong>CULTOS DOMINICAIS</strong></h3>
-            <h5> <FontAwesomeIcon icon={faClock} /> DOMINGO 10 E 18HRS</h5>
+            <h5> <FontAwesomeIcon icon={faClock} /> TODO DOMINGO 10 E 18HRS</h5>
             <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
             <h5>Palmeiras, Belo Horizonte</h5>
           </div>
