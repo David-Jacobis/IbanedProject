1a66bc4f31dd7bd7f41255ebca694c2318f1cbec
David-Jacobis <david.jacobis@outlook.com>
2025-02-10 21:32:13 -0300

    feat: ajustes e adição de novos cards para a página.

--- src/components/programacao/Programacao.jsx
+++ src/components/programacao/Programacao.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Programacao.css";
+import ScrollLinked from "../atoms/scroll/Scroll";
 import Imagem from "../../assets/images/Avalanche.jpg";
-import { Flex} from "antd";
+
+import { Flex } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faLocation, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Programacao = () => {
   return (
     <React.Fragment>
+      <ScrollLinked/>
       <div className="programacao-content">
         <Flex vertical justify="center" align="center">
           <h3>NOSSA PROGRAMAÇÃO</h3>
@@ -13,20 +18,63 @@ const Programacao = () => {
           <h1>PARTE DE ALGO INCRÍVEL</h1>
         </Flex>
         <div className="card">
-      {/* Texto */}
-      <div className="card-text">
-        <h2>CULTO 30 SEMANAS</h2>
-        <h3>QUARTAS-FEIRAS 20HRS</h3>
-        <p>
-          Este é um card simples com um texto e uma imagem ao lado. Ele é responsivo e se ajusta ao tamanho da tela.
-        </p>
-      </div>
+          {/* Texto */}
+          <div className="card-text">
+            <h3><strong>CULTO 30 SEMANAS</strong></h3>
+            <h5> <FontAwesomeIcon icon={faClock} /> QUARTAS-FEIRA 20HRS</h5>
+            <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
+            <h5>Palmeiras, Belo Horizonte</h5>
+          </div>
 
-      {/* Imagem */}
-      <div className="card-image">
-        <img src={Imagem} alt="Imagem do Card" />
-      </div>
-    </div>
+          {/* Imagem */}
+          <div className="card-image">
+            <img src={Imagem} alt="Imagem do Card" />
+          </div>
+        </div>
+        <div className="card">
+          {/* Texto */}
+          <div className="card-text">
+            <h3><strong>CULTO DE ORAÇÃO</strong></h3>
+            <h5> <FontAwesomeIcon icon={faClock} /> QUINTAS-FEIRA 20HRS</h5>
+            <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
+            <h5>Palmeiras, Belo Horizonte</h5>
+          </div>
+
+          {/* Imagem */}
+          <div className="card-image">
+            <img src={Imagem} alt="Imagem do Card" />
+          </div>
+        </div>
+
+        <div className="card">
+          {/* Texto */}
+          <div className="card-text">
+            <h3><strong>CULTO SOW</strong></h3>
+            <h5> <FontAwesomeIcon icon={faClock} /> SÁBADOS 19:30HRS</h5>
+            <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
+            <h5>Palmeiras, Belo Horizonte</h5>
+          </div>
+
+          {/* Imagem */}
+          <div className="card-image">
+            <img src={Imagem} alt="Imagem do Card" />
+          </div>
+        </div>
+
+        <div className="card">
+          {/* Texto */}
+          <div className="card-text">
+            <h3><strong>CULTOS DOMINICAIS</strong></h3>
+            <h5> <FontAwesomeIcon icon={faClock} /> DOMINGO 10 E 18HRS</h5>
+            <h5> <FontAwesomeIcon icon={faLocationDot} /> Av. Raul Mourão Guimarães, 740</h5>
+            <h5>Palmeiras, Belo Horizonte</h5>
+          </div>
+
+          {/* Imagem */}
+          <div className="card-image">
+            <img src={Imagem} alt="Imagem do Card" />
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
