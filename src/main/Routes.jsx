8e12c903de48206fa48a53c947a363be44f1fd34
David-Jacobis <david.jacobis@outlook.com>
2025-02-12 22:21:38 -0300

    feat: adição de células em rota.

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -1,6 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
 import Sobre from "../components/sobre/Sobre";
+import { Routes, Route } from "react-router-dom";
+import Celulas from "../components/celulas/Celulas";
 import Ministerios from "../components/ministerios/Ministerios";
 import Programacao from "../components/programacao/Programacao";
 
@@ -12,6 +13,7 @@ const Rotas = () => {
       <Route path="/Sobre" element={<Sobre />} />
       <Route path="/Ministerios" element={<Ministerios />} />
       <Route path="/Programacao" element={<Programacao />} />
+      <Route path="/Celulas" element={<Celulas/>} />
     </Routes>
   );
 };
